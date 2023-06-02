9c6f8d11db97eec8c891b3f7956961f1d89b07e0
Burhanuddin <burhanuddindalal22@gmail.com>
2023-06-02 12:01:09 -0400

    Add Player And Give Basic Functionality

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,50 @@
-public class Player {
-    public int x;
-    public int y;
-    public int z;
+
+
+public class Player extends Entity {
     double xAngle = 0;
 
-    int n = 20;
     double[] xAxis = new double[]{Math.cos(xAngle), 0, Math.sin(xAngle)};
     double[] yAxis = new double[]{0,1,0};
     double[] normal = new double[]{Math.cos(xAngle + Math.PI / 2), 0, Math.sin(xAngle + Math.PI / 2)};
     boolean right = false, left = false, forward = false, backward = false;
     boolean turnRight = false, turnLeft = false, turnUp = false, turnDown = false;
+
+    public Player(GamePanel gp, double x, double y, double z) {
+        super(gp, x, y, z);
+
+    }
+
+    public void updatePlayer(){
+        updateAxis();
+        if(forward){
+            z += 5 * normal[2];
+            x += 5 * normal[0];
+        }
+        if(backward){
+            z -= 5 * normal[2];
+            x -= 5 * normal[0];
+        }
+        if(right){
+            x += 5 * normal[2];
+            z -= 5 * normal[0];
+
+        }
+        if(left){
+            x -= 5 * normal[2];
+            z += 5 * normal[0];
+
+        }
+        if(turnRight){
+            xAngle = (xAngle - Math.PI / 50) % (2 * Math.PI);
+        }
+        if(turnLeft){
+            xAngle = (xAngle + Math.PI / 50) % (2 * Math.PI);
+        }
+    }
+
+    public void updateAxis(){
+        xAxis = new double[]{Math.cos(xAngle), 0 ,Math.sin(xAngle)};
+        yAxis = new double[]{0,1,0};
+        normal = new double[]{Math.cos(xAngle + Math.PI / 2), 0, Math.sin(xAngle + Math.PI / 2)};
+    }
 }
