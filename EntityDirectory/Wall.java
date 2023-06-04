e581f39ce005fe06f4399bfe957f8987250605b8
Burhanuddin <burhanuddindalal22@gmail.com>
2023-06-04 15:32:19 -0400

    Add Wall to Entity Directory

---
+++ EntityDirectory/Wall.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Wall extends Entity{
+
+
+    public Wall(GamePanel gp, Point[] points, int[] distance){
+        super(gp, points, distance);
+        
+    }
+}
