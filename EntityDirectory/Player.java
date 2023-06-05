094d1e08622c0be4d8103f14f3eefac3745177a8
Burhanuddin Dalal <105935462+theBananadon@users.noreply.github.com>
2023-06-05 11:08:49 -0400

    Give Player Collision

--- EntityDirectory/Player.java
+++ EntityDirectory/Player.java
@@ -2,6 +2,7 @@
 
 public class Player extends Entity {
     double xAngle = 0;
+    int[][] map;
 
     double[] xAxis = new double[]{Math.cos(xAngle), 0, Math.sin(xAngle)};
     double[] yAxis = new double[]{0,1,0};
@@ -9,30 +10,41 @@ public class Player extends Entity {
     boolean right = false, left = false, forward = false, backward = false;
     boolean turnRight = false, turnLeft = false, turnUp = false, turnDown = false;
 
-    public Player(GamePanel gp, double x, double y, double z) {
+    public Player(GamePanel gp, double x, double y, double z, int[][] map) {
         super(gp, x, y, z);
-
+        this.map = map;
     }
 
     public void updatePlayer(){
         if(gp.playState) {
             updateAxis();
             if (forward) {
-                z += 5 * normal[2];
-                x += 5 * normal[0];
+                if(checkCollision(5 * normal[0], 5 * normal[2])){
+                    z += 5 * normal[2];
+                    x += 5 * normal[0];
+                }
             }
             if (backward) {
-                z -= 5 * normal[2];
-                x -= 5 * normal[0];
+                if(checkCollision(-5 * normal[0], -5 * normal[2])){
+                    z -= 5 * normal[2];
+                    x -= 5 * normal[0];
+                }
+                
             }
             if (right) {
-                x += 5 * normal[2];
-                z -= 5 * normal[0];
+                if(checkCollision(5 * normal[2], -5 * normal[0])){
+                    x += 5 * normal[2];
+                    z -= 5 * normal[0];
+                }
+             
 
             }
             if (left) {
-                x -= 5 * normal[2];
-                z += 5 * normal[0];
+                if(checkCollision(-5 * normal[2], 5 * normal[0])){
+                    x -= 5 * normal[2];
+                    z += 5 * normal[0];
+                }
+                
 
             }
             if (turnRight) {
@@ -42,6 +54,15 @@ public class Player extends Entity {
                 xAngle = (xAngle + Math.PI / 50) % (2 * Math.PI);
             }
         }
+    }
+    
+    public boolean checkCollision(double xSpeed, double zSpeed){
+        int finalTileX = (x + xSpeed) / gp.TILE_SIZE;
+        int finalTileZ = (z + zSpeed) / gp.TILE_SIZE;
+        if(map[finalTileX][finalTileZ] == 0){
+            return false;
+        }
+        return true;
     }
 
     public void updateAxis(){
