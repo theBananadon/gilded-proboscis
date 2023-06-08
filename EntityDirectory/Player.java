25a1f02e1092f0c068e84377d636cbe82d2307d9
Burhanuddin Dalal <105935462+theBananadon@users.noreply.github.com>
2023-06-07 20:49:13 -0400

    Give Player Ability to interact with Tasks

--- EntityDirectory/Player.java
+++ EntityDirectory/Player.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 
 public class Player extends Entity {
     double xAngle = 0;
@@ -9,13 +9,19 @@ public class Player extends Entity {
     double[] normal = new double[]{Math.cos(xAngle + Math.PI / 2), 0, Math.sin(xAngle + Math.PI / 2)};
     boolean right = false, left = false, forward = false, backward = false;
     boolean turnRight = false, turnLeft = false, turnUp = false, turnDown = false;
+    boolean isWorkingOnTask = false;
+
+    boolean[] taskObjectCollection = new boolean[3];
+    boolean[] taskCompletion = new boolean[3];
+    boolean isKeyCollected = false;
+    int taskTime = 0;
 
     public Player(GamePanel gp, double x, double y, double z, int[][] map) {
         super(gp, x, y, z);
         this.map = map;
     }
 
-    public void updatePlayer(){
+    public void updatePlayer(Tasks[] tasks, TaskObject[] taskObjects){
         if(gp.playState) {
             updateAxis();
             if (forward) {
@@ -29,14 +35,14 @@ public class Player extends Entity {
                     z -= 2 * normal[2];
                     x -= 2 * normal[0];
                 }
-                
+
             }
             if (right) {
                 if(checkCollision(5 * normal[2], -5 * normal[0])){
                     x += 2 * normal[2];
                     z -= 2 * normal[0];
                 }
-             
+
 
             }
             if (left) {
@@ -44,7 +50,7 @@ public class Player extends Entity {
                     x -= 2 * normal[2];
                     z += 2 * normal[0];
                 }
-                
+
 
             }
             if (turnRight) {
@@ -58,8 +64,10 @@ public class Player extends Entity {
                 z -= 20 * (Math.cos(xAngle) - Math.cos(xAngle + Math.PI / 50));
             }
         }
+        isTaskObjectBeingCollected(taskObjects);
+        isWorkingOnTask(tasks);
     }
-    
+
     public boolean checkCollision(double xSpeed, double zSpeed){
         int finalTileX = (int) ((x + xSpeed) / gp.TILE_SIZE);
         int finalTileZ = (int) ((z + zSpeed) / gp.TILE_SIZE);
@@ -71,4 +79,36 @@ public class Player extends Entity {
         yAxis = new double[]{0,1,0};
         normal = new double[]{Math.cos(xAngle + Math.PI / 2), 0, Math.sin(xAngle + Math.PI / 2)};
     }
+
+    public void isTaskObjectBeingCollected(TaskObject[] taskObjects){
+        for(int i = 0; i < taskObjects.length; i++){
+            if(taskObjects[i] != null){
+                if(checkCollision(taskObjects[i])){
+                    taskObjectCollection[i] = true;
+                    taskObjects[i] = null;
+                }
+            }
+        }
+    }
+
+    public void isWorkingOnTask(Tasks[] tasks) {
+        for(int i = 0; i < tasks.length; i++) {
+            if(tasks[i] != null) {
+                if (tasks[i].tileX == tileX && tasks[i].tileZ == tileZ && isWorkingOnTask) {
+                    taskTime++;
+                    if (taskTime >= 150) {
+                        tasks[i].isCompleted = true;
+                    }
+                } else {
+                    taskTime = 0;
+                }
+            }
+        }
+    }
+
+    public boolean checkCollision(Entity entity){
+        return tileX == entity.tileX && tileZ == entity.tileZ;
+    }
+
+
 }
