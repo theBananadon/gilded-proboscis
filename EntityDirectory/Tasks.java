a562dfddda52347e8e246f030eaaff2b14f0a8fb
Burhanuddin Dalal <105935462+theBananadon@users.noreply.github.com>
2023-06-07 20:48:34 -0400

    Create Task Object

---
+++ EntityDirectory/Tasks.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Tasks extends Entity{
+    boolean isCompleted = false;
+
+    public Tasks(GamePanel gp, Point[] points, int[] distance) {
+        super(gp, points, distance);
+    }
+}
